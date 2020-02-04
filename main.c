3512bbb82fd2750244130c1eecd4497e72603da4
root <root@ns364553.ip-91-121-209.eu>
2020-02-04 07:20:44 +0000

    Maj Main.c

--- main.c
+++ main.c
@@ -21,21 +21,32 @@
 int main(int argc, char const *argv[])
 {
 	int fd = 0;
-	char *line;
+	char *line = NULL;
 	int endl = 1;
+	int i = 1;
+
 
 	if (argc > 1)
 	{
-		fd = open(argv[1], O_RDONLY);
-		endl = 1;
-		while (endl > 0)
+		while (i < argc)
 		{
-			endl = get_next_line(fd, &line);
-			printf("endl: %i | %s\n",endl, line);
-			free(line);
-			line = NULL;
+			fd = open(argv[1], O_RDONLY);
+			endl = 1;
+			while (endl > 0)
+			{
+				endl = get_next_line(fd, &line);
+				if (endl >= 0)
+				{
+					printf("endl: %i | %s\n",endl, line);
+					free(line);
+					line = NULL;
+				}
+				else
+					printf("endl: %i\n", endl);
+			}
+			close(fd);
+			i++;
 		}
-		close(fd);
 	}
 	return (0);
 }
